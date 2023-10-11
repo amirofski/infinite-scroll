4c33c276f5c014704fc5e999f3d5749357e468cb
amirofski <amir.devel@gmail.com>
2023-10-11 13:43:57 +0330

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,44 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ViewChildren, QueryList, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'infinite-scroll';
+
+  @ViewChildren('post') posts!: QueryList<ElementRef>;
+
+  constructor() {}
+
   @HostListener('window:resize', ['$event'])
-  onResize(event) {
+  onResize(event: Event) {
     // Code to handle resize
   }
 
   @HostListener('window:scroll', ['$event'])
-  onScroll(event) {
+  onScroll(event: Event) {
     // Code to handle scroll
+  }
+
+  ngAfterViewInit() {
+    let options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: [0, 0.5, 1] // 0, 50%, and 100% visibility
+    };
+
+    let observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0.5) {
+          // Code to handle 50% visibility
+        }
+      });
+    }, options);
+
+    this.posts.forEach(post => {
+      observer.observe(post.nativeElement);
+    });
   }
 }
